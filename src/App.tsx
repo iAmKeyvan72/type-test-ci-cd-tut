9ac9d81c6e8b16eb76a4b99f5c6dd2d3fdd8bde9
Keyvan Mahmoudi <iamkeyvan72@gmail.com>
2022-09-15 19:20:24 +0200

    Test netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Test Normal Success!</div>;
+  return <div>Test Normal Lets see if the netlify changes it or not!?!</div>;
 };
 
 export default App;
