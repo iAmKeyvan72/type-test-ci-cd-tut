a21ce8de65f6ea306955c0f1a58de3a789f5dd5e
Keyvan Mahmoudi <iamkeyvan72@gmail.com>
2022-09-15 19:24:11 +0200

    Make it success test!

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App';
 describe('tests the App root component', () => {
   it('should render the awesome test', () => {
     render(<App />);
-    expect(screen.getByText(/Normalllll/)).toBeInTheDocument();
+    expect(screen.getByText(/Normal/)).toBeInTheDocument();
   });
 });
