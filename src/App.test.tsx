f63aaa540e975f9f950e785000cc25853cb8fe80
Keyvan Mahmoudi <iamkeyvan72@gmail.com>
2022-09-16 13:02:35 +0200

    Add a simple TDD form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,65 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 describe('tests the App root component', () => {
-  it('should render the awesome test', () => {
+  it('should correctly display the created item on the screen', async () => {
+    const user = userEvent.setup();
+
     render(<App />);
-    expect(screen.getByText(/Normal/)).toBeInTheDocument();
+    const itemDescription = screen.getByRole('textbox');
+    const createButton = screen.getByRole('button');
+
+    await user.click(itemDescription);
+    await user.keyboard('Hoy');
+    await user.click(createButton);
+
+    expect(screen.getByRole('textbox')).toHaveValue('');
+    expect(screen.getByText('Hoy')).toBeInTheDocument();
+
+    await user.click(itemDescription);
+    await user.keyboard('Here is the second one');
+    await user.click(createButton);
+
+    expect(screen.getByRole('textbox')).toHaveValue('');
+    expect(screen.getByText('Here is the second one')).toBeInTheDocument();
+  });
+
+  it('should not add empty item to the list', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    const itemDescription = screen.getByRole('textbox');
+    const createButton = screen.getByRole('button');
+
+    await user.click(itemDescription);
+    await user.keyboard('Hoy');
+    await user.click(createButton);
+
+    expect(screen.getByRole('textbox')).toHaveValue('');
+
+    await user.click(createButton); // Tries add an empty item
+    expect(screen.getAllByRole('listitem')).toHaveLength(1);
+  });
+
+  it('should allow user to have similar name items', async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    const inputElement = screen.getByRole('textbox');
+    const createButton = screen.getByRole('button');
+
+    await user.click(inputElement);
+    await user.keyboard('Hoy');
+    await user.click(createButton);
+
+    // Make similar items
+    await user.click(inputElement);
+    await user.keyboard('Hoy');
+    await user.click(createButton);
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Input } from '@mui/material';
 
 const App = () => {
-  return <div>Test Normal Lets see if the netlify changes it or not if failed!?!</div>;
+  const [userInput, setUserInput] = useState<string>('');
+  const [savedInputs, setSavedInputs] = useState<string[]>([]);
+
+  return (
+    <div>
+      <Input value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+      <Button
+        onClick={() => {
+          if (userInput) {
+            setSavedInputs((prevList) => [...prevList, userInput]);
+            setUserInput('');
+          }
+        }}
+      >
+        Create
+      </Button>
+
+      <ul>
+        {savedInputs?.map((str, i) => (
+          <li key={str + i}>{str}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
